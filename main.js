6b1a9cd3b96cde20e9d3bdf7e2e27125a17b20bd
tankalxat34 <tankalxat34@gmail.com>
2022-06-28 20:29:46 +0300

    Изучил функции

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const person = {
-    city: 'New York',
-    name: 'Alexander',
-    digit: 5
+function myFn(a, b) {
+    /* некоторая функция */
+    let c = a + b;
+    return c;
 }
 
-const person2 = Object.assign({}, person)
-
-person.digit = 18
-console.log(person.digit)
-console.log(person2.digit)
+console.log(myFn(5, 3))
