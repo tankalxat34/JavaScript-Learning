f7eecee9464024700dd0358c03e2bff41e03ceae
tankalxat34 <tankalxat34@gmail.com>
2022-06-29 20:10:03 +0300

    До конца изучил функции и привел примеры

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-let person = {
-    city: 'Moscow',
-    name: 'Alexander'
+// обычная функция
+function f1(a, b = 1) {
+    return a * b;
 }
 
-const greeting = `Привет! Меня зовут ${person.name} и я живу в ${person.city}`
+console.log('Результат f1')
+console.log(f1(5, 2))   // 10
+console.log(f1(5))      // 5
 
-console.log(greeting)
+
+// анонимное функциональное выражение
+const f2 = function(a, b = 1) {
+    return a * b;
+}
+
+console.log('Результат f2')
+console.log(f2(5, 2))   // 10
+console.log(f2(5))      // 5
+
+
+// стрелочные функции
+const f3 = (a, b = 1) => {
+    return a * b;
+}
+
+console.log('Результат f3')
+console.log(f3(5, 2))   // 10
+console.log(f3(5))      // 5
