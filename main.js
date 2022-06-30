5bed3f635b77fded168b6d92cdfa1863cd01e703
tankalxat34 <tankalxat34@gmail.com>
2022-06-30 17:03:16 +0300

    Изучил switch ... case ... default

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-const userProfile = {
-    firstname: 'Alexander',
-    commentsQty: 23,
-    hasSignedAgreement: false
+let monthNumber = 1
+
+switch (monthNumber) {
+    case 1:
+        console.log("Это январь")
+        break
+    default:
+        console.log("Я не знаю этот месяц")
 }
 
-/* 
-Берем свойства с именами такими то из значения, которое будет передано в аргумент функции
-
-На основе указанных свойств из будут взяты значения этих свойств из переданного объекта, которые могут использоваться внутри функции
-*/
-const userInfo = ({ firstname, commentsQty }) => {
-    if (!commentsQty) {
-        return `User ${firstname} has no comments`
-    }
-    return `User ${firstname} has ${commentsQty} comments`
-}
-
-console.log(userInfo(userProfile)) // 'User Alexander has 23 comments'
