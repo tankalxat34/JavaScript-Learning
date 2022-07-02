d801fc33e429c30589a60ce89060718a57b5885e
tankalxat34 <tankalxat34@gmail.com>
2022-07-02 17:46:33 +0300

    Начал изучать React по бесплатному курсу

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const timerPromise = () =>
-    new Promise((resolve, reject) =>
-        setTimeout(() => resolve(), 2000)
+const getData = url => 
+    new Promise((resolve, reject) => 
+        fetch(url)
+            .then(response => response.json())
+            .then(json => resolve(json))
+            .catch(error => reject(error))
     )
 
-const asyncFn = async () => {
-    console.log('Timer starts')
-    const startTime = performance.now()
-    await timerPromise()
-    const endTime = performance.now()
-    console.log('Timer ended', endTime - startTime)
+function resolved(data) {
+    console.log(data.id)
+    console.log('  Total downloads', data.total_downloads)
 }
 
-asyncFn()
+const url = 'https://api.pepy.tech/api/v2/projects/UploadgramPyAPI'
+getData(url)
+    .then(data => resolved(data))
+    .catch(error => console.log(error.message))
+
