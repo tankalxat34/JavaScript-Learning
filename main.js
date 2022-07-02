2294d5ea4ced3e989ea551754f59a69cc5ea12d1
tankalxat34 <tankalxat34@gmail.com>
2022-07-02 16:14:40 +0300

    Изучил промисы

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-class Comment {
-    constructor(text) {
-        /* конструктор класса. Определяет сам класс. Аналог __init__ класса в Python */
-        this.text = text
-        this.votesQty = 0
-    }
+function resolvedPromise(json) {
+    console.log(json[0].title)
+    console.log(json[0].completed)
 
-    upvote() {
-        this.votesQty += 1
-    }
-
-    static mergeComments(first, second) {
-        return `${first} ${second}`
-    }
+    console.table(json[0])
 }
 
-const firstComment = new Comment('first comment')
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+        console.log(response)
+        return response.json()
+    })      // вызовется, если промис выполнен. Метод json возвращает промис, содержащий ответ от сервера
+    .then(json => resolvedPromise(json))        // вызовется, если промис выполнен. На экран выведется объект, созданный на основе данных, полученных на прыдыдущем объекте
+    .catch(error => console.error(error))   // вызовется, если промис вернул ошибку. На экран выведем эту ошибку
 
-console.log(Comment.prototype === firstComment.__proto__)
